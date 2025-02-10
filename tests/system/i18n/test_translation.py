d3d6f27993dec20fdc05eb1f650e607d4116b98c
Jacob Sevlie <jsevlie@gmail.com>
2025-02-10 11:16:07 -0600

    add `faker` to dev group

--- tests/system/i18n/test_translation.py
+++ tests/system/i18n/test_translation.py
@@ -6,25 +6,30 @@ from system.i18n.translation import preload_translations
 
 
 @pytest.fixture
-def core_path(app):
-    return os.path.join(app.root_path, "modules", "core", "lang")
+def module_name(faker):
+    return str(faker.file_name(extension=None))
 
 
-def test_preload_translations__translations_exist(core_path, fs):
-    fs.create_file(os.path.join(core_path, "es.json"), contents='{"Settings": "Configuración"}')
-    fs.create_file(os.path.join(core_path, "fr.json"), contents='{"Settings": "Paramètres"}')
+@pytest.fixture
+def module_path(app, module_name):
+    return os.path.join(app.root_path, "modules", module_name, "lang")
+
+
+def test_preload_translations__translations_exist(module_path, fs, module_name):
+    fs.create_file(os.path.join(module_path, "es.json"), contents='{"Settings": "Configuración"}')
+    fs.create_file(os.path.join(module_path, "fr.json"), contents='{"Settings": "Paramètres"}')
     actual_translations = preload_translations()
-    assert actual_translations["es"]["core"]["Settings"] == "Configuración"
-    assert actual_translations["fr"]["core"]["Settings"] == "Paramètres"
+    assert actual_translations["es"][module_name]["Settings"] == "Configuración"
+    assert actual_translations["fr"][module_name]["Settings"] == "Paramètres"
 
 
-def test_preload_translations__translations_empty(core_path, fs):
-    fs.create_file(os.path.join(core_path, "es.json"), contents="{}")
+def test_preload_translations__translations_empty(module_path, fs, module_name):
+    fs.create_file(os.path.join(module_path, "es.json"), contents="{}")
     actual_translations = preload_translations()
-    assert actual_translations["es"]["core"] == {}
+    assert actual_translations["es"][module_name] == {}
 
 
-def test_preload_translations__translations_missing(core_path, fs):
-    fs.create_file(os.path.join(core_path, "es.txt"))
+def test_preload_translations__translations_missing(module_path, fs):
+    fs.create_file(os.path.join(module_path, "es.txt"))
     actual_translations = preload_translations()
     assert actual_translations == {}
